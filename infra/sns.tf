ca4168e355f95aea909574c3f84bd0ad97860d4e
marvinlanhenke <lanhenke@googlemail.com>
2024-10-29 12:12:01 +0100

    feat: add sns subscription

--- infra/sns.tf
+++ infra/sns.tf
@@ -2,3 +2,9 @@ resource "aws_sns_topic" "aws_polly_post_new_posts" {
   name         = "aws-polly-post-new-posts"
   display_name = "AWS-Polly-Post New Posts"
 }
+
+resource "aws_sns_topic_subscription" "new_posts_subscription" {
+  topic_arn = aws_sns_topic.aws_polly_post_new_posts.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.convert_to_audio.arn
+}
