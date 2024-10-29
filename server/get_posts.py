4513e0f17729588c47facd04ee58e8f1744c6f43
marvinlanhenke <lanhenke@googlemail.com>
2024-10-29 13:42:08 +0100

    fix: lambda get_posts

--- server/get_posts.py
+++ server/get_posts.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import boto3
@@ -7,25 +8,23 @@ dynamodb = boto3.resource("dynamodb")
 
 
 def lambda_handler(event, context):
-    record_id = event.get("recordId")
+    params = event["queryStringParameters"]
+    record_id = params.get("recordId") if params else None
 
     print(f"retrieving items for recordId: {record_id}")
 
-    if not record_id:
-        return {"statusCode": 400, "error": "You must provide a valid 'record_id'"}
-
     table = dynamodb.Table(os.environ["DB_TABLE_NAME"])  # type: ignore
 
-    if record_id == "*":
+    if not record_id:
         items = table.scan()
     else:
         items = table.query(KeyConditionExpression=Key("id").eq(record_id))
 
     return {
         "statusCode": 200,
-        "body": {
+        "body": json.dumps({
             "message": "Successfully retrieved items",
             "recordId": record_id,
             "items": items["Items"],
-        },
+        }),
     }
