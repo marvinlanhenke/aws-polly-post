5ef85299034296b9847d1c9f6726494839ff9340
marvinlanhenke <lanhenke@googlemail.com>
2024-10-29 12:07:22 +0100

    chore: add error handling

--- server/new_posts.py
+++ server/new_posts.py
@@ -13,25 +13,32 @@ def lambda_handler(event, context):
     text = event.get("text")
 
     if not voice or not text:
-        raise ValueError("You must provide a valid 'voice' and 'text'")
+        return {
+            "statusCode": 400,
+            "error": "You must provide a valid 'voice' and 'text'",
+        }
 
     print(
         f"creating new DynamoDB record with ID: {record_id}, text: {text}, voice: {voice}"
     )
 
-    dynamodb.put_item(
-        TableName=os.environ["DB_TABLE_NAME"],
-        Item={
-            "id": {"S": record_id},
-            "text": {"S": text},
-            "voice": {"S": voice},
-            "status": {"S": "PROCESSING"},
-        },
-    )
+    try:
+        dynamodb.put_item(
+            TableName=os.environ["DB_TABLE_NAME"],
+            Item={
+                "id": {"S": record_id},
+                "text": {"S": text},
+                "voice": {"S": voice},
+                "status": {"S": "PROCESSING"},
+            },
+        )
 
-    sns.publish(TopicArn=os.environ["SNS_TOPIC_ARN"], Message=record_id)
+        sns.publish(TopicArn=os.environ["SNS_TOPIC_ARN"], Message=record_id)
 
-    return {
-        "statusCode": 200,
-        "body": {"message": "Successfully created a new post", "id": record_id},
-    }
+        return {
+            "statusCode": 200,
+            "body": {"message": "Successfully created a new post", "id": record_id},
+        }
+    except Exception as e:
+        print(e)
+        return {"statusCode": 500, "error": "InternalServerError"}
