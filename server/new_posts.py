940c11637af600fb1299c861a8bc22c434377df5
marvinlanhenke <lanhenke@googlemail.com>
2024-10-29 09:47:23 +0100

    feat: add lambda for new_posts

--- server/new_posts.py
+++ server/new_posts.py
@@ -1,4 +1,37 @@
-def lambda_handler(event, context):
-    print("Hello, World!")
+import os
+import uuid
 
-    return {"statusCode": 200}
+import boto3
+
+dynamodb = boto3.client("dynamodb")
+sns = boto3.client("sns")
+
+
+def lambda_handler(event, context):
+    record_id = str(uuid.uuid4())
+    voice = event.get("voice")
+    text = event.get("text")
+
+    if not voice or not text:
+        raise ValueError("You must provide a valid 'voice' and 'text'")
+
+    print(
+        f"creating new DynamoDB record with ID: {record_id}, text: {text}, voice: {voice}"
+    )
+
+    dynamodb.put_item(
+        TableName=os.environ["DB_TABLE_NAME"],
+        Item={
+            "id": {"S": record_id},
+            "text": {"S": text},
+            "voice": {"S": voice},
+            "status": {"S": "PROCESSING"},
+        },
+    )
+
+    sns.publish(TopicArn=os.environ["SNS_TOPIC_ARN"], Message=record_id)
+
+    return {
+        "statusCode": 200,
+        "body": {"message": "Successfully created a new post", "id": record_id},
+    }
