6f44e7ae5305e6c8cf9152edae3c59b8b5a9d3a1
marvinlanhenke <lanhenke@googlemail.com>
2024-10-29 12:25:36 +0100

    feat: add stub lambda 'get_posts'

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,8 +1,3 @@
-resource "aws_cloudwatch_log_group" "log_group_new_posts" {
-  name              = "/aws/lambda/${aws_lambda_function.new_posts.function_name}"
-  retention_in_days = 1
-}
-
 data "archive_file" "archive_new_posts" {
   type        = "zip"
   source_file = "${path.module}/../server/new_posts.py"
@@ -70,5 +65,35 @@ resource "null_resource" "sam_metadata_aws_lambda_function_convert_to_audio" {
     resource_type        = "ZIP_LAMBDA_FUNCTION"
     original_source_code = "${path.module}/../server"
     built_output_path    = "${path.module}/../server/build/convert_to_audio.zip"
+  }
+}
+
+data "archive_file" "archive_get_posts" {
+  type        = "zip"
+  source_file = "${path.module}/../server/get_posts.py"
+  output_path = "${path.module}/../server/build/get_posts.zip"
+}
+
+resource "aws_lambda_function" "get_posts" {
+  filename         = "${path.module}/../server/build/get_posts.zip"
+  function_name    = "get_posts"
+  role             = aws_iam_role.iam_for_lamba.arn
+  source_code_hash = data.archive_file.archive_get_posts.output_base64sha256
+  runtime          = "python3.12"
+  handler          = "get_posts.lambda_handler"
+
+  environment {
+    variables = {
+      DB_TABLE_NAME = "${aws_dynamodb_table.aws_polly_post_posts.arn}"
+    }
+  }
+}
+
+resource "null_resource" "sam_metadata_aws_lambda_function_get_posts" {
+  triggers = {
+    resource_name        = "aws_lambda_function.get_posts"
+    resource_type        = "ZIP_LAMBDA_FUNCTION"
+    original_source_code = "${path.module}/../server"
+    built_output_path    = "${path.module}/../server/build/get_posts.zip"
   }
 }
